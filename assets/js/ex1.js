c7dc5e2effb527b567ddd6dbb39a6a8888b74c07
CoyezFabien <coyez.fabien@gmail.com>
2020-11-20 17:41:07 +0100

    ex1jQuery

---
+++ assets/js/ex1.js
@@ -0,0 +1,23 @@
+$(function()
+{
+    $('#text').hide();
+});
+/*le signe $  est une propriété  que l'on appelle des alias avec jQuery, les develloppeurs utilises le $ car c'est plus rapide à marquer
+le $ va permettre de mentionner les selecteurs associé a cet element possèdent un attribut pour récupérer une ID c'est $('#...')tres important le # sinon cela ne fonctionne pas
+.hide permet de cacher l'element qui possède l'attribut
+*/
+
+// $(function(){
+    
+//         $(this).hide();
+//     });
+// });
+
+// ce format est la base pour faire fonctionner le jQuiery 
+
+// $(document).ready(function(){
+//     $("#p1").click(function(){
+//         $(this).hide();
+//     });
+// });
+// le $(document).ready est remplacé par le $ pour simplifier le code
